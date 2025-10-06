4a977ced90cd142706a926dfbfa59f498cf8720e
uosq <lboxwallytf2@gmail.com>
2025-10-06 18:00:34 -0300

    Add README.md

---
+++ README.md
@@ -0,0 +1,9 @@
+# Radar
+
+![Square dark mode](assets/dark%20mode%20square.png)
+
+![Square light mode](assets/light%20mode%20square.png)
+
+![Circle dark mode](assets/dark%20mode%20circle.png)
+
+![Circle light mode](assets/light%20mode%20circle.png)
--- src/config.lua
+++ src/config.lua
@@ -2,10 +2,10 @@ local config = {
     size = 200,
     icon_size = 12,
     zoom = 0.1, -- 10% zoom
-    circular = false,
+    circular = true,
     healthbar = true,
     background_transparency = 250,
-    light_mode = false,
+    light_mode = true,
     world_locked = false,
     icon_outline = true,
 }
